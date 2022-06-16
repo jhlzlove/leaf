ea8fe60db78a9fe4f1c40c3fd831919bcc878b5f
jhlz <jhlzlove@163.com>
2022-06-17 07:32:51 +0800

    测试冲突

--- market-admin/src/main/java/com/market/MarketApplication.java
+++ market-admin/src/main/java/com/market/MarketApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序 冲突测试提交
- *
+ * 测试冲突提交2
  * @author ruoyi
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
