0671fcbbaa30a83d1c311a022ad42714047a3686
jhlz <jhlzlove@163.com>
2022-08-10 17:26:53 +0800

    fix: 🐛 fix 访问swagger地址拒绝连接，添加全局常量类

--- market-vben-example/src/main/java/com/market/example/MarketVbenExampleApplication.java
+++ market-vben-example/src/main/java/com/market/example/MarketVbenExampleApplication.java
@@ -1,6 +1,5 @@
 package com.market.example;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,13 +8,10 @@ import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class MarketVbenExampleApplication {
-
-    @Value("${server.port}")
-    private static String port;
     public static void main(String[] args) throws UnknownHostException {
         SpringApplication.run(MarketVbenExampleApplication.class, args);
         System.out.println("swagger 地址：http://" + Inet4Address.getLocalHost().getHostAddress() +
-                ":" + port + "/swagger-ui/index.html");
+                ":8080/swagger-ui/index.html");
     }
 
 }
---
+++ market-vben-example/src/main/java/com/market/example/constant/GlobalConstant.java
@@ -0,0 +1,9 @@
+package com.market.example.constant;
+
+/**
+ * @author clf
+ * @time 2022/8/10 17:19
+ * @desc: GlobalConstant
+ */
+public class GlobalConstant {
+}
