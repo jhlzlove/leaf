e154f68941256e8da1ae13131fd35aea52bcc5ea
jhlz <jhlzlove@163.com>
2022-08-29 15:16:17 +0800

    refactor: update main
    use constants replace string

--- market-vben-example/src/main/java/com/example/MarketVbenExampleApplication.java
+++ market-vben-example/src/main/java/com/example/MarketVbenExampleApplication.java
@@ -1,19 +1,22 @@
 package com.example;
 
+import com.example.common.constant.GlobalConstants;
 import com.example.common.utils.IpUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.management.*;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
 //扫描mapper
 @MapperScan("com.example.mapper")
 public class MarketVbenExampleApplication {
-    public static void main(String[] args) throws UnknownHostException {
-        SpringApplication.run(MarketVbenExampleApplication.class, args);
-        System.out.println("swagger 地址：http://" + IpUtil.getLocalUrl() + ":7777/swagger-ui/index.html");
-    }
 
+    public static void main(String[] args) throws UnknownHostException, ReflectionException, AttributeNotFoundException, InstanceNotFoundException, MBeanException, MalformedObjectNameException {
+        SpringApplication.run(MarketVbenExampleApplication.class, args);
+        System.out.println("swagger 地址：http://" + IpUtil.getLocalUrl() + ":" +
+                GlobalConstants.PORT + GlobalConstants.SWAGGER_SUFFIX);
+    }
 }
