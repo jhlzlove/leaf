96dc27838af3babfca1dee1035c61cd0036f1d1d
jhlz <jhlzlove@163.com>
2022-08-29 12:29:13 +0800

    chore(market-image): add new market-img

---
+++ market-vben-example/src/main/java/com/example/common/log/LogHomeConfig.java
@@ -0,0 +1,7 @@
+package com.example.common.log;/**
+ * @author jhlz
+ * @time 2022/8/29 9:47
+ * @desc: LogHomeConfig
+ */
+public class LogHomeConfig {
+}
---
+++ market-vben-example/src/test/java/com/example/JdkApiTest.java
@@ -0,0 +1,7 @@
+package com.example;/**
+ * @author jhlz
+ * @time 2022/8/29 9:50
+ * @desc: JdkApiTest
+ */
+public class JdkApiTest {
+}
