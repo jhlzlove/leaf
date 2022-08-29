a3cc37658359569597e95db7af299f139f4ede63
jhlz <jhlzlove@163.com>
2022-08-29 15:14:56 +0800

    feat: add new menu service

--- market-vben-example/src/main/java/com/example/service/SysMenuService.java
+++ market-vben-example/src/main/java/com/example/service/SysMenuService.java
@@ -1,2 +1,14 @@
-package com.example.service.impl;public interface SysMenuService {
+package com.example.service;
+
+import com.example.domain.SysMenu;
+
+import java.util.List;
+
+public interface SysMenuService {
+    /**
+     * 获取菜单列表
+     *
+     * @return
+     */
+    List<SysMenu> getMenuList();
 }
--- market-vben-example/src/main/java/com/example/service/impl/SysMenuServiceImpl.java
+++ market-vben-example/src/main/java/com/example/service/impl/SysMenuServiceImpl.java
@@ -1,7 +1,30 @@
-package com.example.service.impl;/**
+package com.example.service.impl;
+
+import com.example.domain.SysMenu;
+import com.example.mapper.SysMenuMapper;
+import com.example.service.SysMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
  * @author jhlz
  * @time 2022/8/29 12:58
  * @desc: SysMenuServiceImpl
  */
-public class SysMenuServiceImpl {
+@Service
+public class SysMenuServiceImpl implements SysMenuService {
+
+    @Override
+    public List<SysMenu> getMenuList() {
+        return menuMapper.listMenu();
+    }
+
+    private final SysMenuMapper menuMapper;
+
+    @Autowired
+    public SysMenuServiceImpl(SysMenuMapper menuMapper) {
+        this.menuMapper = menuMapper;
+    }
 }
