d438885a4b2ba690ed6d03596bd5d820cf8ff784
jhlz <jhlzlove@163.com>
2022-08-29 12:39:23 +0800

    refactor(jwttokeninterceptor): allow no jwt released

--- market-vben-example/src/main/java/com/example/common/filter/JwtAuthenticationTokenFilter.java
+++ market-vben-example/src/main/java/com/example/common/filter/JwtAuthenticationTokenFilter.java
@@ -1,12 +1,8 @@
 package com.example.common.filter;
 
-import com.example.common.constant.GlobalConstants;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -27,14 +23,16 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 1. 获取并解析 token
-        String token = request.getHeader("token");
+        /*String token = request.getHeader("token");
         if (ObjectUtils.isEmpty(token)) {
             filterChain.doFilter(request, response);
             return;
         }
         Claims body = Jwts.parser().setSigningKey(GlobalConstants.JWT_KEY)
                 .parseClaimsJws(token).getBody();
-        logger.info("Parse Claims：{}", body.toString());
+        logger.info("Parse Claims：{}", body.toString());*/
+        // 直接放行
+        filterChain.doFilter(request, response);
     }
 
 
