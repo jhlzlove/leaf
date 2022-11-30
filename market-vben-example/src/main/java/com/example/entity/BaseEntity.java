3d868c8d6595b037b898c91456289f021e5ec9f9
jhlz <jhlzlove@163.com>
2022-11-30 19:43:42 +0800

    feat: add sql

---
+++ market-vben-example/src/main/java/com/example/entity/BaseEntity.java
@@ -0,0 +1,7 @@
+package com.example.entity;/**
+ * 
+ * @author: jhlz
+ * @since: 2022/11/26 15:33:42
+ */
+public class BaseEntity {
+}
--- sql/vben_market.sql
+++ sql/vben_market.sql
@@ -12,72 +12,72 @@
  Target Server Version : 150000 (150000)
  File Encoding         : 65001
 
- Date: 05/11/2022 16:12:05
+ Date: 30/11/2022 19:42:08
 */
 
-
--- ----------------------------
--- Table structure for vben_account
--- ----------------------------
-DROP TABLE IF EXISTS "public"."vben_account";
-CREATE TABLE "public"."vben_account" (
-  "id" int4 NOT NULL,
-  "account_name" varchar(255) COLLATE "pg_catalog"."default",
-  "nick_name" varchar(255) COLLATE "pg_catalog"."default",
-  "password" varchar(255) COLLATE "pg_catalog"."default",
-  "sex" varchar(255) COLLATE "pg_catalog"."default",
-  "create_time" date,
-  "update_time" date,
-  "create_by" varchar(255) COLLATE "pg_catalog"."default",
-  "update_by" date
-)
-;
-COMMENT ON COLUMN "public"."vben_account"."account_name" IS '用户名';
-COMMENT ON COLUMN "public"."vben_account"."nick_name" IS '昵称';
-COMMENT ON COLUMN "public"."vben_account"."password" IS '密码';
-COMMENT ON COLUMN "public"."vben_account"."sex" IS '性别';
-COMMENT ON COLUMN "public"."vben_account"."create_time" IS '创建时间';
-COMMENT ON COLUMN "public"."vben_account"."update_time" IS '更新时间';
-COMMENT ON COLUMN "public"."vben_account"."create_by" IS '创建者';
-COMMENT ON COLUMN "public"."vben_account"."update_by" IS '更新者';
-COMMENT ON TABLE "public"."vben_account" IS '帐户表';
-
--- ----------------------------
--- Records of vben_account
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for vben_dept
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."vben_dept";
 CREATE TABLE "public"."vben_dept" (
-  "id" int4 NOT NULL,
-  "dept_name" varchar(255) COLLATE "pg_catalog"."default",
-  "dept_code" varchar(255) COLLATE "pg_catalog"."default",
-  "dept_pcode" varchar(255) COLLATE "pg_catalog"."default",
-  "leader" varchar(255) COLLATE "pg_catalog"."default",
-  "phone" varchar(255) COLLATE "pg_catalog"."default",
+  "id" int8 NOT NULL,
+  "dept_name" varchar(50) COLLATE "pg_catalog"."default",
+  "code" varchar(50) COLLATE "pg_catalog"."default",
+  "pcode" varchar(50) COLLATE "pg_catalog"."default",
+  "leader" varchar(50) COLLATE "pg_catalog"."default",
+  "phone" varchar(11) COLLATE "pg_catalog"."default",
   "create_time" date,
   "update_time" date,
-  "create_by" varchar(255) COLLATE "pg_catalog"."default",
-  "update_by" date,
+  "create_by" varchar(50) COLLATE "pg_catalog"."default",
+  "update_by" varchar(50) COLLATE "pg_catalog"."default",
   "remark" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
 COMMENT ON COLUMN "public"."vben_dept"."dept_name" IS '部门名称';
-COMMENT ON COLUMN "public"."vben_dept"."dept_code" IS '部门编码';
-COMMENT ON COLUMN "public"."vben_dept"."dept_pcode" IS '部门父级编码';
+COMMENT ON COLUMN "public"."vben_dept"."code" IS '部门编码';
+COMMENT ON COLUMN "public"."vben_dept"."pcode" IS '部门父级编码';
 COMMENT ON COLUMN "public"."vben_dept"."leader" IS '负责人';
 COMMENT ON COLUMN "public"."vben_dept"."phone" IS '负责人电话';
 COMMENT ON COLUMN "public"."vben_dept"."create_time" IS '创建时间';
 COMMENT ON COLUMN "public"."vben_dept"."update_time" IS '更新时间';
 COMMENT ON COLUMN "public"."vben_dept"."create_by" IS '创建人';
-COMMENT ON COLUMN "public"."vben_dept"."update_by" IS '负责人电话';
+COMMENT ON COLUMN "public"."vben_dept"."update_by" IS '更新人';
 COMMENT ON COLUMN "public"."vben_dept"."remark" IS '备注';
 COMMENT ON TABLE "public"."vben_dept" IS '部门表';
 
 -- ----------------------------
 -- Records of vben_dept
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for vben_dict
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."vben_dict";
+CREATE TABLE "public"."vben_dict" (
+  "id" int4 NOT NULL,
+  "dict_type" varchar(255) COLLATE "pg_catalog"."default",
+  "dict_name" varchar(255) COLLATE "pg_catalog"."default",
+  "dict_code" varchar(255) COLLATE "pg_catalog"."default",
+  "create_time" date,
+  "update_time" date,
+  "create_by" varchar(50) COLLATE "pg_catalog"."default",
+  "update_by" varchar(50) COLLATE "pg_catalog"."default",
+  "remark" varchar(255) COLLATE "pg_catalog"."default"
+)
+;
+COMMENT ON COLUMN "public"."vben_dict"."dict_type" IS '类型';
+COMMENT ON COLUMN "public"."vben_dict"."dict_name" IS '名称';
+COMMENT ON COLUMN "public"."vben_dict"."dict_code" IS '编码';
+COMMENT ON COLUMN "public"."vben_dict"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."vben_dict"."update_time" IS '更新时间';
+COMMENT ON COLUMN "public"."vben_dict"."create_by" IS '创建人';
+COMMENT ON COLUMN "public"."vben_dict"."update_by" IS '更新人';
+COMMENT ON COLUMN "public"."vben_dict"."remark" IS '备注';
+COMMENT ON TABLE "public"."vben_dict" IS '数据字典';
+
+-- ----------------------------
+-- Records of vben_dict
 -- ----------------------------
 
 -- ----------------------------
@@ -115,19 +115,25 @@ COMMENT ON TABLE "public"."vben_menu" IS '菜单表';
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."vben_role";
 CREATE TABLE "public"."vben_role" (
-  "id" int4 NOT NULL,
+  "id" int8 NOT NULL,
   "role_name" varchar(255) COLLATE "pg_catalog"."default",
+  "role_code" varchar(255) COLLATE "pg_catalog"."default",
   "create_time" date,
   "update_time" date,
-  "create_by" varchar(255) COLLATE "pg_catalog"."default",
-  "update_by" date
+  "create_by" varchar(50) COLLATE "pg_catalog"."default",
+  "update_by" varchar(50) COLLATE "pg_catalog"."default",
+  "remark" varchar(255) COLLATE "pg_catalog"."default",
+  "status" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
 COMMENT ON COLUMN "public"."vben_role"."role_name" IS '角色名称';
+COMMENT ON COLUMN "public"."vben_role"."role_code" IS '角色';
 COMMENT ON COLUMN "public"."vben_role"."create_time" IS '创建时间';
 COMMENT ON COLUMN "public"."vben_role"."update_time" IS '更新时间';
 COMMENT ON COLUMN "public"."vben_role"."create_by" IS '创建人';
 COMMENT ON COLUMN "public"."vben_role"."update_by" IS '更新人';
+COMMENT ON COLUMN "public"."vben_role"."remark" IS '备注';
+COMMENT ON COLUMN "public"."vben_role"."status" IS '状态 == 1 ? 启用 : 禁用';
 COMMENT ON TABLE "public"."vben_role" IS '角色表';
 
 -- ----------------------------
@@ -139,25 +145,32 @@ COMMENT ON TABLE "public"."vben_role" IS '角色表';
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."vben_user";
 CREATE TABLE "public"."vben_user" (
-  "id" int4 NOT NULL,
-  "user_name" varchar(255) COLLATE "pg_catalog"."default",
-  "sex" varchar(255) COLLATE "pg_catalog"."default",
-  "birthday" varchar(255) COLLATE "pg_catalog"."default",
-  "card_code" varchar(255) COLLATE "pg_catalog"."default",
-  "age" int4,
+  "id" int8 NOT NULL,
+  "name" varchar(20) COLLATE "pg_catalog"."default",
+  "sex" bool DEFAULT false,
+  "birthday" varchar(64) COLLATE "pg_catalog"."default",
+  "identity_card" varchar(20) COLLATE "pg_catalog"."default",
+  "age" int2,
   "address" varchar(255) COLLATE "pg_catalog"."default",
-  "phone" varchar(255) COLLATE "pg_catalog"."default",
+  "phone" char(11) COLLATE "pg_catalog"."default",
   "create_time" date,
   "update_time" date,
-  "create_by" varchar(255) COLLATE "pg_catalog"."default",
-  "update_by" date,
-  "remark" varchar(255) COLLATE "pg_catalog"."default"
+  "create_by" varchar(50) COLLATE "pg_catalog"."default",
+  "update_by" varchar(50) COLLATE "pg_catalog"."default",
+  "remark" varchar(255) COLLATE "pg_catalog"."default",
+  "user_name" varchar(64) COLLATE "pg_catalog"."default",
+  "password" varchar(64) COLLATE "pg_catalog"."default",
+  "last_login" date,
+  "login_device" varchar(255) COLLATE "pg_catalog"."default",
+  "nick_name" varchar(54) COLLATE "pg_catalog"."default",
+  "avatar" varchar(255) COLLATE "pg_catalog"."default",
+  "status" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
-COMMENT ON COLUMN "public"."vben_user"."user_name" IS '用户名';
-COMMENT ON COLUMN "public"."vben_user"."sex" IS '性别';
+COMMENT ON COLUMN "public"."vben_user"."name" IS '姓名';
+COMMENT ON COLUMN "public"."vben_user"."sex" IS '性别(true:女:男)';
 COMMENT ON COLUMN "public"."vben_user"."birthday" IS '出生日期';
-COMMENT ON COLUMN "public"."vben_user"."card_code" IS '身份证号';
+COMMENT ON COLUMN "public"."vben_user"."identity_card" IS '身份证号';
 COMMENT ON COLUMN "public"."vben_user"."age" IS '年龄';
 COMMENT ON COLUMN "public"."vben_user"."address" IS '居住地址';
 COMMENT ON COLUMN "public"."vben_user"."phone" IS '联系电话';
@@ -166,6 +179,13 @@ COMMENT ON COLUMN "public"."vben_user"."update_time" IS '更新时间';
 COMMENT ON COLUMN "public"."vben_user"."create_by" IS '创建人';
 COMMENT ON COLUMN "public"."vben_user"."update_by" IS '更新人';
 COMMENT ON COLUMN "public"."vben_user"."remark" IS '备注';
+COMMENT ON COLUMN "public"."vben_user"."user_name" IS '登录账户';
+COMMENT ON COLUMN "public"."vben_user"."password" IS '密码';
+COMMENT ON COLUMN "public"."vben_user"."last_login" IS '最后登录时间';
+COMMENT ON COLUMN "public"."vben_user"."login_device" IS '登录设备';
+COMMENT ON COLUMN "public"."vben_user"."nick_name" IS '昵称';
+COMMENT ON COLUMN "public"."vben_user"."avatar" IS '头像';
+COMMENT ON COLUMN "public"."vben_user"."status" IS '1？启用 : 禁用';
 COMMENT ON TABLE "public"."vben_user" IS '用户信息表';
 
 -- ----------------------------
@@ -177,9 +197,9 @@ COMMENT ON TABLE "public"."vben_user" IS '用户信息表';
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."vben_user_dept";
 CREATE TABLE "public"."vben_user_dept" (
-  "id" int4 NOT NULL,
-  "dept_id" int4,
-  "user_id" int4
+  "id" int8 NOT NULL,
+  "dept_id" int8,
+  "user_id" int8
 )
 ;
 COMMENT ON COLUMN "public"."vben_user_dept"."dept_id" IS '部门id';
@@ -191,32 +211,32 @@ COMMENT ON TABLE "public"."vben_user_dept" IS '用户部门关联表';
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for vebn_account_role
+-- Table structure for vebn_user_role
 -- ----------------------------
-DROP TABLE IF EXISTS "public"."vebn_account_role";
-CREATE TABLE "public"."vebn_account_role" (
-  "id" int4 NOT NULL,
-  "account_id" int4,
-  "role_id" int4
+DROP TABLE IF EXISTS "public"."vebn_user_role";
+CREATE TABLE "public"."vebn_user_role" (
+  "id" int8 NOT NULL,
+  "account_id" int8,
+  "role_id" int8
 )
 ;
-COMMENT ON COLUMN "public"."vebn_account_role"."account_id" IS '用户id';
-COMMENT ON COLUMN "public"."vebn_account_role"."role_id" IS '角色id';
-COMMENT ON TABLE "public"."vebn_account_role" IS '账户角色关联表';
+COMMENT ON COLUMN "public"."vebn_user_role"."account_id" IS '用户id';
+COMMENT ON COLUMN "public"."vebn_user_role"."role_id" IS '角色id';
+COMMENT ON TABLE "public"."vebn_user_role" IS '账户角色关联表';
 
 -- ----------------------------
--- Records of vebn_account_role
+-- Records of vebn_user_role
 -- ----------------------------
-
--- ----------------------------
--- Primary Key structure for table vben_account
--- ----------------------------
-ALTER TABLE "public"."vben_account" ADD CONSTRAINT "vben_account_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
 -- Primary Key structure for table vben_dept
 -- ----------------------------
 ALTER TABLE "public"."vben_dept" ADD CONSTRAINT "vben_dept_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table vben_dict
+-- ----------------------------
+ALTER TABLE "public"."vben_dict" ADD CONSTRAINT "vben_dict_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
 -- Primary Key structure for table vben_menu
@@ -239,6 +259,6 @@ ALTER TABLE "public"."vben_user" ADD CONSTRAINT "vben_user_pkey" PRIMARY KEY ("i
 ALTER TABLE "public"."vben_user_dept" ADD CONSTRAINT "vben_user_dept_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
--- Primary Key structure for table vebn_account_role
+-- Primary Key structure for table vebn_user_role
 -- ----------------------------
-ALTER TABLE "public"."vebn_account_role" ADD CONSTRAINT "vebn_account_role_pkey" PRIMARY KEY ("id");
+ALTER TABLE "public"."vebn_user_role" ADD CONSTRAINT "vebn_account_role_pkey" PRIMARY KEY ("id");
