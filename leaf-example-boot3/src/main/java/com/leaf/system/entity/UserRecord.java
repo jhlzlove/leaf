92732b79d1cff7c114fd39fbfa08c70b2026e89c
jhlz <jhlz99520@163.com>
2023-02-27 16:39:42 +0800

    chore: upgrade spring-boot dependency to 3.0.3

---
+++ leaf-example-boot3/src/main/java/com/leaf/system/entity/UserRecord.java
@@ -0,0 +1,7 @@
+package com.leaf.system.entity;/**
+ * @author jhlz
+ * @since 2023/2/27 9:09
+ * @version 
+ */
+public record UserRecord() {
+}
---
+++ leaf-example-boot3/src/test/java/com/leaf/simpletest/RecordTest.java
@@ -0,0 +1,7 @@
+package com.leaf.simpletest;/**
+ * @author jhlz
+ * @since 2023/2/27 9:10
+ * @version 
+ */
+public class RecordTest {
+}
