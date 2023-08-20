99739be459e5864058cea1bf30de7d1b502b6b07
jhlz <jhlz99520@163.com>
2023-08-20 21:25:03 +0800

    feat: update some feat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,31 @@
+import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    // Java 插件
     java
     // spring boot version control
     id("org.springframework.boot") version "3.1.2"
-    // 该插件用于管理 Spring Boot 项目中依赖项的版本。
-    // 它是 Spring Boot 官方提供的 Gradle 插件之一，可以帮助开发人员更轻松地管理项目依赖项。
-    // 该插件的作用是统一管理项目依赖项的版本，使项目的依赖项版本更加清晰明确，避免出现依赖冲突。
+    // Spring Boot 官方提供的 Gradle 插件之一，可以帮助开发人员更轻松地管理项目依赖项。统一管理项目依赖项的版本，使项目的依赖项版本更加清晰明确，避免出现依赖冲突。
     id("io.spring.dependency-management") version "1.1.2"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
 }
 
-subprojects {
+group = "com.leaf"
+version = "0.0.1"
+
+allprojects {
+    // 所有项目使用的插件
     apply {
+        plugin("kotlin")
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
     }
-}
-
-group = "com.leaf"
-version = "0.0.1"
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-// 配置所有项目（包括子项目，拥有最高优先级），所有子项目继承相同的配置。
-allprojects {
-
-
-    // 依赖下载源设置
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    // 依赖下载源
     repositories {
         // 本地 maven 仓库
         mavenLocal()
@@ -43,41 +37,32 @@ allprojects {
         //     isAllowInsecureProtocol = true
         // }
     }
-}
 
-// 依赖
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    /* spring security */
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    /* spring web */
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    /* spring aop */
-    implementation("org.springframework.boot:spring-boot-starter-aop")
-    /* spring data jpa */
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    /* validation */
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    /* postgresql */
-    implementation("org.postgresql:postgresql:${pgVersion}")
-    /* java jwt */
-    // https://github.com/auth0/java-jwt
-    implementation("com.auth0:java-jwt:${jwtVersion}")
-    /* 缩略图 */
-    implementation("net.coobird:thumbnailator:0.4.19")
+    // 所有项目的公共依赖
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+    tasks {
+        // kt 编译选项
+        withType<KotlinCompile> {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xplugin=org.jetbrains.kotlin.allopen:" +
+                        "annotation=org.springframework.stereotype.Service")
+                freeCompilerArgs += "-Xjsr305=strict"
+                jvmTarget = "17"
+            }
+        }
+        // java 编译器编译字符集
+        withType<JavaCompile> {
+            options.encoding = "UTF-8"
+            options.compilerArgs.add("-parameters")
+        }
 
-// kt 编译选项
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        withType<Jar> {
+            enabled = true
+        }
     }
 }
 
-// java 编译字符编码
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    java
-    kotlin("jvm") version "1.8.22"
+    `kotlin-dsl`
 }
 
 repositories {
