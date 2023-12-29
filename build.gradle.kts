c6bc02725e3d362285671f4757ae9c9839a99429
jhlz <jhlz99520@163.com>
2023-12-29 14:02:55 +0800

    feat: update gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,35 @@
+val mavenUrl: String by project.extra
 plugins {
-    java
+    `java-library`
     // spring boot version control
-    id("org.springframework.boot") version "3.1.4"
+    id("org.springframework.boot") version "3.2.1"
     // Spring Boot 官方提供的 Gradle 插件之一，可以帮助开发人员更轻松地管理项目依赖项。统一管理项目依赖项的版本，使项目的依赖项版本更加清晰明确，避免出现依赖冲突。
-    id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
-    idea
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
-version = "1.0.0"
-group = "com.leaf"
+subprojects {
 
-allprojects {
-    // 所有项目使用的插件
+    group = "code.leaf"
+    version = "1.0.0"
+
     apply {
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
-        plugin("java")
+        plugin("java-library")
     }
-
     java {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
     // 依赖下载源
     repositories {
-        // 本地 maven 仓库
         mavenLocal()
-        // 腾讯云仓库
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-        // 如果项目中需要从 http 下载依赖，打开下面的设置
-        // maven {
-        //     url = uri("http://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-        //     isAllowInsecureProtocol = true
-        // }
+        maven(mavenUrl)
     }
-
-    // 所有项目的公共依赖
     dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-reflect")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
     }
-
     tasks {
         // java 编译器编译字符集
         withType<JavaCompile> {
@@ -52,4 +39,3 @@ allprojects {
         }
     }
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.name = "leaf"
-// 模块前面加入冒号表示相对路径
-include(":leaf-system")
-include(":leaf-code-generation")
+include("leaf-system")
+include("leaf-code-generation")
+include("leaf-common")
